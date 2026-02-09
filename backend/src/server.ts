1b7362a35608e66d589519011d2e277b5a141784
aimableshyaka <shyakaaimable07@gmail.com>
2026-02-09 16:47:14 +0200

    email notification confllicy resolved

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,15 @@
 import dotenv from 'dotenv';
+dotenv.config();
+
 import { app } from './app';
 import connectDB from './config/db';
-
-dotenv.config();
+import notificationRoutes from "./routes/notification.routes";
 
 const PORT = process.env.PORT || 5000;
 
 // Connect to Database
 connectDB();
-
+app.use("/api/notify", notificationRoutes);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
